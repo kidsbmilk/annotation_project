69b2ca3a338fe9939f03ce23703f1a9f20b81049
zhangzhen5 <zhangzhen5@xiaomi.com>
2018-09-14 20:46:28 +0800

    add annotations

--- guava-26.0-annotation/guava/src/com/google/common/annotations/Beta.java
+++ guava-26.0-annotation/guava/src/com/google/common/annotations/Beta.java
@@ -33,6 +33,10 @@ import java.lang.annotation.Target;
  *
  *
  * @author Kevin Bourrillion
+ *
+ * 表示公共API（公共类，方法或字段）在将来的版本中会发生不兼容的更改，甚至删除。 带有此注释的API不受其包含库所做的任何兼容性保证的限制。 请注意，此注释的存在并不意味着所讨论的API的质量或性能，只是它不是“API冻结”的事实。
+
+<p> <i>应用程序</ i>通常可以安全地依赖于beta API，但代价是升级期间的一些额外工作。 但是，对于<i>库</ i>（它们包含在用户的CLASSPATH中，在库开发人员的控制之外）通常是不可取的。
  */
 @Retention(RetentionPolicy.CLASS)
 @Target({
