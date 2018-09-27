3e0ed924c744606dd90b7fea03309ffe76e2e12a
zhangzhen5 <zhangzhen5@xiaomi.com>
2018-09-27 14:57:49 +0800

    add annotations about AggretateFutureState

--- guava-26.0-annotation/guava/src/com/google/common/util/concurrent/AggregateFuture.java
+++ guava-26.0-annotation/guava/src/com/google/common/util/concurrent/AggregateFuture.java
@@ -165,7 +165,9 @@ abstract class AggregateFuture<InputT, OutputT> extends AbstractFuture.TrustedFu
         }
       } else {
         // We'll only call the callback when all futures complete, regardless of whether some failed
+        // 我们只会在所有futures完成时调用回调，无论是否有一些失败
         // Hold off on calling setOneValue until all complete, so we can share the same listener
+        // 暂停调用setOneValue直到全部完成，这样我们就可以共享同一个监听器
         for (ListenableFuture<? extends InputT> listenable : futures) {
           listenable.addListener(this, directExecutor());
         }
@@ -177,6 +179,9 @@ abstract class AggregateFuture<InputT, OutputT> extends AbstractFuture.TrustedFu
      * throwable if it is an {@link Error} or if {@link #allMustSucceed} is {@code true}, the
      * throwable did not cause this future to fail, and it is the first time we've seen that
      * particular Throwable.
+     * 如果{@link #allMustSucceed}为真，则使用给定的Throwable失败这个future。
+     * 另外，如果它是{@link Error}或者如果{@link #allMustSucceed}是{@code true}，则记录throwable，
+     * throwable不会导致此future失败，这是我们第一次看到特别的throwable。
      */
     private void handleException(Throwable throwable) {
       checkNotNull(throwable);
@@ -186,12 +191,14 @@ abstract class AggregateFuture<InputT, OutputT> extends AbstractFuture.TrustedFu
       if (allMustSucceed) {
         // As soon as the first one fails, throw the exception up.
         // The result of all other inputs is then ignored.
+          // 一旦第一个失败，抛出异常。 然后忽略所有其他输入的结果。
         completedWithFailure = setException(throwable);
         if (completedWithFailure) {
           releaseResourcesAfterFailure();
         } else {
           // Go up the causal chain to see if we've already seen this cause; if we have, even if
           // it's wrapped by a different exception, don't log it.
+            // 走上因果链，看看我们是否已经看到了这个原因; 如果我们有，即使它被一个不同的异常包装，也不要记录它。
           firstTimeSeeingThisException = addCausalChain(getOrInitSeenExceptions(), throwable);
         }
       }
@@ -219,6 +226,7 @@ abstract class AggregateFuture<InputT, OutputT> extends AbstractFuture.TrustedFu
     private void handleOneInputDone(int index, Future<? extends InputT> future) {
       // The only cases in which this Future should already be done are (a) if it was cancelled or
       // (b) if an input failed and we propagated that immediately because of allMustSucceed.
+        // 此Future应该已经完成的仅有的情况是（a）如果它被取消或（b）如果某个输入失败并且由于设置了allMustSucceed导致我们立即传播它。
       checkState(
           allMustSucceed || !isDone() || isCancelled(),
           "Future was done before all dependencies completed");
@@ -229,10 +237,12 @@ abstract class AggregateFuture<InputT, OutputT> extends AbstractFuture.TrustedFu
           if (future.isCancelled()) {
             // clear running state prior to cancelling children, this sets our own state but lets
             // the input futures keep running as some of them may be used elsewhere.
+              // 在取消孩子之前清除运行状态，这设置了我们自己的状态，但让输入的futures继续运行，因为其中一些可能在其他地方使用。
             runningState = null;
             cancel(false);
           } else {
             // We always get the result so that we can have fail-fast, even if we don't collect
+              // 我们总是得到结果，以便我们可以快速失败，即使我们不收集
             InputT result = getDone(future);
             if (collectsValues) {
               collectOneValue(allMustSucceed, index, result);
@@ -259,6 +269,7 @@ abstract class AggregateFuture<InputT, OutputT> extends AbstractFuture.TrustedFu
     private void processCompleted() {
       // Collect the values if (a) our output requires collecting them and (b) we haven't been
       // collecting them as we go. (We've collected them as we go only if we needed to fail fast)
+        // 在以下情况下，收集这些值：如果（a）我们的产出需要收集它们，（b）我们没有收集它们。 （我们只有在需要快速失败时才收集它们）
       if (collectsValues & !allMustSucceed) {
         int i = 0;
         for (ListenableFuture<? extends InputT> listenable : futures) {
@@ -274,6 +285,9 @@ abstract class AggregateFuture<InputT, OutputT> extends AbstractFuture.TrustedFu
      * immediately release resources we no longer need); additionally, the future will release its
      * reference to {@link RunningState}, which should free all associated memory when all the
      * futures complete and the listeners are released.
+     * 监听器隐含地保留对{@link RunningState}的引用，因为它们是内部类，因此我们在此处释放资源以及allMustSucceed = true情况
+     * （即，当future失败时，我们立即释放资源，因为我们不再需要它们了）; 此外，future将释放对{@link RunningState}的引用，
+     * 当所有future完成并释放监听器时，它应释放所有相关内存。
      *
      * <p>TODO(user): Write tests for memory retention
      */
@@ -285,9 +299,11 @@ abstract class AggregateFuture<InputT, OutputT> extends AbstractFuture.TrustedFu
 
     /**
      * Called only if {@code collectsValues} is true.
+     * 仅在{@code collectctsValues}为true时才调用。
      *
      * <p>If {@code allMustSucceed} is true, called as each future completes; otherwise, called for
      * each future when all futures complete.
+     * <p>如果{@code allMustSucceed}为true，则在每个future完成时被调用; 否则，当所有future完成时，对每个future调用此方法。
      */
     abstract void collectOneValue(boolean allMustSucceed, int index, @Nullable InputT returnValue);
 
@@ -306,6 +322,9 @@ abstract class AggregateFuture<InputT, OutputT> extends AbstractFuture.TrustedFu
          * where this isn't true, but we ignore it: If we record an exception, then someone calls
          * initCause() on it, and then we examine it again, we'll conclude that we've seen the whole
          * chain before when it fact we haven't. But this should be rare.)
+         * 我们已经看到了这一点，所以我们也看到了它的原因。 无需重新添加它们。
+         * （有一种情况不是这样，但我们忽略它：如果我们记录一个异常，那么有人在其上调用initCause（），然后我们再次检查它，
+         * 我们将得出结论，我们已经看到了整个链，但是事实上我们没有。但这应该是罕见的。）
          */
         return false;
       }
--- guava-26.0-annotation/guava/src/com/google/common/util/concurrent/AggregateFutureState.java
+++ guava-26.0-annotation/guava/src/com/google/common/util/concurrent/AggregateFutureState.java
@@ -29,10 +29,13 @@ import java.util.logging.Logger;
 /**
  * A helper which does some thread-safe operations for aggregate futures, which must be implemented
  * differently in GWT. Namely:
+ * 一个为聚合future做一些线程安全操作的帮助类，必须在GWT中以不同方式实现。即：
  *
  * <ul>
  *   <li>Lazily initializes a set of seen exceptions
+ *       Lazily初始化了一组看到的异常
  *   <li>Decrements a counter atomically
+ *       原子地减少计数器
  * </ul>
  */
 @GwtCompatible(emulated = true)
@@ -40,6 +43,8 @@ import java.util.logging.Logger;
 abstract class AggregateFutureState {
   // Lazily initialized the first time we see an exception; not released until all the input futures
   // & this future completes. Released when the future releases the reference to the running state
+  // 延迟初始化，直到我们第一次看到异常时才初始化它们; 直到所有输入future和这个future完成后才会释放它们。
+  // 在future释放对运行状态的引用时才会释放此对象。
   private volatile Set<Throwable> seenExceptions = null;
 
   private volatile int remaining;
@@ -61,12 +66,14 @@ abstract class AggregateFutureState {
       // getDeclaredField to throw a NoSuchFieldException when the field is definitely there.
       // For these users fallback to a suboptimal implementation, based on synchronized. This will
       // be a definite performance hit to those users.
+        // 这里的设计参考AbstracfFuture中的静态初始化块。
       thrownReflectionFailure = reflectionFailure;
       helper = new SynchronizedAtomicHelper();
     }
     ATOMIC_HELPER = helper;
     // Log after all static init is finished; if an installed logger uses any Futures methods, it
     // shouldn't break in cases where reflection is missing/broken.
+      // 所有静态init完成后记录; 如果安装的记录器使用任何Futures方法，则在反射丢失/损坏的情况下不应该破坏。
     if (thrownReflectionFailure != null) {
       log.log(Level.SEVERE, "SafeAtomicHelper is broken!", thrownReflectionFailure);
     }
@@ -82,16 +89,22 @@ abstract class AggregateFutureState {
      * approach would be for each caller CAS it from null to a Set populated with its exception. But
      * there's another race: If the first thread fails with an exception and a second thread
      * immediately fails with the same exception:
+     * seenExceptions的初始化必须比我们想要的更复杂。 简单的方法是每个调用者CAS从null到填充了它的异常的Set。
+     * 但还有另一种竞争：如果第一个线程因异常而失败，第二个线程立即失败并出现相同的异常：
      *
      * Thread1: calls setException(), which returns true, context switch before it can CAS
      * seenExceptions to its exception
+     * Thread1：调用setException()，它返回true，在CAS操作将seenExceptions设置为它的异常之前发生了上下文切换。
      *
      * Thread2: calls setException(), which returns false, CASes seenExceptions to its exception,
      * and wrongly believes that its exception is new (leading it to logging it when it shouldn't)
+     * Thread2：调用setException()，返回false，CAS操作将seenExceptions设置为它的异常，并错误地认为它的异常是新的（导致它在不应该的时候记录它）
      *
      * Our solution is for threads to CAS seenExceptions from null to a Set population with _the
      * initial exception_, no matter which thread does the work. This ensures that seenExceptions
      * always contains not just the current thread's exception but also the initial thread's.
+     * 我们的解决方案是针对CAS操作seenExceptions的线程，将seenExceptions从null设置为初始异常，无论哪个线程都起作用。
+     * 这可以确保seenExceptions不仅包含当前线程的异常，还包含初始线程的异常。
      */
     Set<Throwable> seenExceptionsLocal = seenExceptions;
     if (seenExceptionsLocal == null) {
@@ -100,6 +113,7 @@ abstract class AggregateFutureState {
        * Other handleException() callers may see this as soon as we publish it. We need to populate
        * it with the initial failure before we do, or else they may think that the initial failure
        * has never been seen before.
+       * 其他handleException（）调用者可以在我们发布它时立即看到它。 在我们做之前，我们需要用初始失败来填充它，否则他们可能会认为以前从未见过初始失败。
        */
       addInitialException(seenExceptionsLocal);
 
@@ -107,8 +121,10 @@ abstract class AggregateFutureState {
       /*
        * If another handleException() caller created the set, we need to use that copy in case yet
        * other callers have added to it.
+       * 如果另一个handleException（）调用者创建了该集合，我们需要使用该副本，以防其他调用者添加到该集合中。
        *
        * This read is guaranteed to get us the right value because we only set this once (here).
+       * 这个读取保证给我们正确的值，因为我们只设置了一次（这里）。
        */
       seenExceptionsLocal = seenExceptions;
     }
